aa7bf8b15e7835789c9c017026ba6fa8d308b815
Alison Zerbe <alisonkzerbe@gmail.com>
2019-04-22 17:20:46 -0700

    styles article info

--- src/ArticleInfoContainer.js
+++ src/ArticleInfoContainer.js
@@ -6,8 +6,8 @@ class ArticleInfoContainer extends Component {
     render() {
         return (
             <div class="display">
-                <ArticleInfoComponent title={this.props.title} descr={this.props.descr}/>
-                <AuthorInfoComponent class="autor-info" authImg={this.props.authImg} author={this.props.author} date={this.props.date} readTime={this.props.readTime} />
+                <ArticleInfoComponent title={this.props.title} descr={this.props.descr} />
+                <AuthorInfoComponent authImg={this.props.authImg} author={this.props.author} date={this.props.date} readTime={this.props.readTime} />
             </div>
         );
     }
--- src/AuthorInfoComponent.js
+++ src/AuthorInfoComponent.js
@@ -1,23 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import bookmark from './tag.png';
 import logo from './logo.svg';
 import './App.css';
 class AuthorInfoComponent extends Component {
     render() {
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-        const monthNum = parseInt(this.props.date.substring(5,7));
-        const month = months[monthNum-1];
-        const day = this.props.date.substring(8,10).replace(/^0+/,'');
-    
+        const monthNum = parseInt(this.props.date.substring(5, 7));
+        const month = months[monthNum - 1];
+        const day = this.props.date.substring(8, 10).replace(/^0+/, '');
+
         return (
             <div class="author-info">
-                <img src={this.props.authImg} id="auth-img"/>
                 <div id="auth-details">
-                    <div>{this.props.author}</div>
-                    <div>{`${month} ${day}`}</div>
-                    <div>{`${this.props.readTime} min read `}</div>
+                    <img src={this.props.authImg} id="auth-img" />
+                    <div>
+                        <div class="auth-data">{this.props.author}</div>
+                        <div class="auth-data">{`${month} ${day} ∙ ${this.props.readTime} min read`}</div>
+                    </div>
                 </div>
-                <img src={logo} class="bookmark" />
+                <img src={bookmark} class="bookmark" />
             </div>
         );
     }
